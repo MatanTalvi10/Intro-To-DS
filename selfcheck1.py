9bbc532cf50aa2390f880976fe84b395c259a9c2
MatanTalvi10 <matantalbi@gmail.com>
2023-03-17 16:31:32 +0200

    Q11

---
+++ selfcheck1.py
@@ -0,0 +1,10 @@
+import numpy as np
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[5, 6]])
+c = np.concatenate((a, b), axis=0)
+print(c)
+t = a
+a = np.arange(1,20,2).reshape(5,2)
+print(a)
+b = np.reshape(np.arange(1, 21, 2), (5, 2), order='F')
+print(b)
