0f6953f6afff9fbe8c12f1002d20588d8e19c35a
MatanTalvi10 <matantalbi@gmail.com>
2023-03-19 21:05:23 +0200

    Tocheck

--- selfcheck1.py
+++ selfcheck1.py
@@ -1,10 +1,5 @@
 import numpy as np
 a = np.array([[1, 2], [3, 4]])
-b = np.array([[5, 6]])
-c = np.concatenate((a, b), axis=0)
-print(c)
-t = a
-a = np.arange(1,20,2).reshape(5,2)
-print(a)
-b = np.reshape(np.arange(1, 21, 2), (5, 2), order='T')
-print(b)
+b = a
+print(a*b)
+print(np.dot(a,b))
